4ac05dddaf64e00bcb51d2a4efc7ede1fc855e3f
Philip Warren <philwarren1@gmail.com>
2020-07-28 15:53:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(name)
 end
 def summon_captain_planet(array)
   array = []
-  array.upcase
+  array.map! {|array| array.capitalize}
 end
 
 def long_planeteer_calls# code an argument here
