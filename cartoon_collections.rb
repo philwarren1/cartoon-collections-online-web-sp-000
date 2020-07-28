03851bb57685d107fd0bb8f1c976047562fce158
Philip Warren <philwarren1@gmail.com>
2020-07-28 15:47:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(name)
   hash = Hash.new
 %w(name).each_with_index { |name, index|
   hash[name] = index} 
+  puts name
 end
 def summon_captain_planet# code an argument here
   # Your code here
