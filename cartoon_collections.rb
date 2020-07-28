ae686b326ca8eaa096cac40c516ad794e1662c5f
Philip Warren <philwarren1@gmail.com>
2020-07-28 15:55:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 def summon_captain_planet(array)
   array = []
   array.map! {|array| array.capitalize}
-  
+  array.each {|array| puts array + '!'}
 end
 
 def long_planeteer_calls# code an argument here
